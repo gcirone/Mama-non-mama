7152a7aed8cb481422ad1d14e634a52305c9d6c3
Gianluca Cirone <gianluca.cirone@gmail.com>
2015-03-06 13:00:08 +0100

    update minisite

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log('This would be the main JS file.');
+$(window).ready(init);
+
+function init(){
+    $('.preview_image img')
+        .mouseover(overImage)
+        .mousemove(moveImage)
+        .mouseout(outImage);
+}
+
+function overImage(e) {
+    $('#image-fly').remove();
+    var image = $(this).clone();
+    image.attr('id', 'image-fly').addClass('fly');
+    $('body').append(image);
+}
+
+function moveImage(e){
+    var image = $('#image-fly');
+    image.css('top', e.pageY-250);
+    image.css('left', e.pageX+10);
+}
+function outImage(e){
+    $('#image-fly').remove();
+}
